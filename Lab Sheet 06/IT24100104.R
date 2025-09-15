da5d962afcbbd3504600da22af0ca511102399e4
IT24100104 <it24100104@my.sliit.lk>
2025-09-15 11:51:28 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24100104.R
@@ -0,0 +1,14 @@
+dbinom(40,44,0.92)
+pbinom(35,44,0.92,lower.tail=TRUE)
+1-pbinom(37,44,0.92,lower.tail = TRUE)
+pbinom(37,44,0.92,lower.tail = FALSE)
+pbinom(42,44,0.92,lower.tail = TRUE)-pbinom(39,44,0.92,lower.tail = TRUE)
+dpois(6,5)
+ppois(6,5,lower.tail = FALSE)
+setwd("C:\\Users\\MSI\\OneDrive\\Desktop\\IT24100104")
+n<-50
+p<-0.85
+pbinom(46,50,0.85)
+# i. Random variable X = number of calls received in an hour
+lamda<-12
+ppois(15,12)
